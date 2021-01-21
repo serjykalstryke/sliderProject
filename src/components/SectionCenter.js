fa71d087e6ea438528e4e74801612d3caa032c06
David Stinnett <davidstinnett@icloud.com>
2021-01-20 19:11:30 -0500

    componentized some of the parts but somewhat limited by the way project is set up. Probably won't be an issue in a real project as this is one slider component

---
+++ src/components/SectionCenter.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import data from "../data";
+import Content from "./Content";
+
+function SectionCenter() {
+	return <Content />;
+}
+
+export default SectionCenter;
